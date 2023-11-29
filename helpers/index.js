97066187068b37e7587070b12a729a9627c6e91d
Ruslan-Kostenko <123192295+Ruslan-Kostenko@users.noreply.github.com>
2023-11-30 01:05:24 +0200

    completed

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,9 @@
 const HttpError = require("./HttpError");
 const CtrlWrapper = require("./CtrlWrapper");
+const MongooseError = require("./MongooseError")
 
 module.exports = {
   HttpError,
   CtrlWrapper,
+  MongooseError,
 };

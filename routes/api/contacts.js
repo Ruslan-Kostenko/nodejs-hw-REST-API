11e69cf436c39f59251f3b93d3972014af95d9e3
Ruslan-Kostenko <123192295+Ruslan-Kostenko@users.noreply.github.com>
2023-12-07 15:32:18 +0200

    add jimp and verify user isOwner

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const ctrl = require("../../controllers/contactsDecor");
-const { validateBody, isValidId, authent } = require("../../middlewares");
+const { validateBody, isValidId, authent, isOwner } = require("../../middlewares");
 const {schemas} = require("../../models/contact");
 
 const router = express.Router();
@@ -8,14 +8,14 @@ const router = express.Router();
 
 router.get("/", authent, ctrl.getAll);
 
-router.get("/:contactId", authent, isValidId, ctrl.getById);
+router.get("/:contactId", authent, isValidId, isOwner, ctrl.getById);
 
 router.post("/", authent, validateBody(schemas.addSchema), ctrl.postCont);
 
-router.delete("/:contactId", authent, isValidId, ctrl.deleteCont);
+router.delete("/:contactId", authent, isValidId, isOwner, ctrl.deleteCont);
 
-router.put("/:contactId", authent, isValidId, validateBody(schemas.addSchema), ctrl.putCont);
+router.put("/:contactId", authent, isValidId, isOwner, validateBody(schemas.addSchema), ctrl.putCont);
 
-router.patch("/:contactId/favorite", authent, isValidId, validateBody(schemas.favoriteSchema), ctrl.patchCont);
+router.patch("/:contactId/favorite", authent, isValidId, isOwner, validateBody(schemas.favoriteSchema), ctrl.patchCont);
 
 module.exports = router;
